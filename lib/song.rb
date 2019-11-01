7bcfcad4a71247ffe3faac668fdb36eb37f5da57
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-01 13:37:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,5 +16,21 @@ class Song
     @@all << self 
     song 
   end 
+  
+  def self.new_by_name(song)
+    song = Song.new  
+  end
+  
+  def self.create_by_name(song)
+    song = Song.new
+    @@all << self
+  end 
+  
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end 
+  
+  def self.find_or_create_by_name(name)
+  end
 
 end
