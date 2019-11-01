7e4e01f06fe2a9bcc8970ca521069bb213e6aaec
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-01 14:35:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,7 +54,14 @@ class Song
   end 
   
   def self.create_from_filename(filename)
+    row = filename
+    data = row.split(" - ")
+    artist_name = data[0]
+    song_name = data[1]
     
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
     
     song.save
     song 
